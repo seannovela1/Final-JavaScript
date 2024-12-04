5e78e515a6f80b1a7a8d994de01d23aa93e9e838
seannovela1 <seannovela5484@my.whccd.edu>
2024-12-03 20:54:06 -0800

    grabs the DOM elements for the "Play Again" button, the scoreboard for both players, and the game-over message, storing them in variables for later interaction in the game.

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 const squares = Array.from(document.querySelectorAll('.game-square'));
 const turnElement = document.getElementById('turn');
 const resultMessage = document.getElementById('result-message');
+const playAgainButton = document.getElementById('button-play-again');
+const scoreboardX = document.getElementById('scoreboard-x');
+const scoreboardO = document.getElementById('scoreboard-o');
+const gameOverMessage = document.getElementById('game-over-message');
