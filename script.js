e6b132284f00fffe9f727666dc40175041dc3d2a
seannovela1 <seannovela5484@my.whccd.edu>
2024-12-03 20:56:03 -0800

    currentPlayer = 'X': Sets Player X to start. gameActive = true: Indicates the game is active. boardState = ['', '', '', '', '', '', '', '', '']: Represents an empty 3x3 game board.

--- script.js
+++ script.js
@@ -5,3 +5,8 @@ const playAgainButton = document.getElementById('button-play-again');
 const scoreboardX = document.getElementById('scoreboard-x');
 const scoreboardO = document.getElementById('scoreboard-o');
 const gameOverMessage = document.getElementById('game-over-message');
+
+
+let currentPlayer = 'X';
+let gameActive = true;
+let boardState = ['', '', '', '', '', '', '', '', ''];
